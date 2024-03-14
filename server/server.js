ab45233e55675b71bb54763c002aaaada62168e0
Vishal Kumar <107054166+VI5H4L@users.noreply.github.com>
2024-03-15 00:21:29 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ const { PORT } = process.env;
 // create server app
 const app = express();
 
-app.use(cors({ origin: 'https://wisewaste.vercel.app' }));
-app.options('*', cors());
+app.use(cors());
+// app.options('*', cors());
 app.use(bodyParser());
 
 // /authentication/signup
