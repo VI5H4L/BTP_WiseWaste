7a898ad4e4c979963ec45f06c05a694b72ba11c9
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-03-16 03:15:00 +0530

    new

--- server/server.js
+++ server/server.js
@@ -5,19 +5,8 @@ const bodyParser = express.json;
 const cors = require("cors");
 const { PORT } = process.env;
 
-// create server app
 const app = express();
-app.options('*', cors());
-// app.use(cors());
-
-app.use(cors({
-  origin: 'https://wisewaste.vercel.app' // Allow only this origin
-}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://wisewaste.vercel.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(cors());
 app.use(bodyParser());
 
 // /authentication/signup
