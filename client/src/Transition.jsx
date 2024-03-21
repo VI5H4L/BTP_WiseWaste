2c526ee180bbfe5739f6f0ea2b09957503b35d17
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-03-21 19:54:52 +0530

    Scroll preserved for each page (0,0)

--- client/src/Transition.jsx
+++ client/src/Transition.jsx
@@ -1,6 +1,14 @@
 import { motion } from "framer-motion";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Transition = ({ children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
