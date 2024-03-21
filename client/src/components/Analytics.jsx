f48db29e87c8f560e65f4dc33125f65368fa61f2
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-03-21 18:31:37 +0530

    Implemented REACT-QUERY to fetch data

--- client/src/components/Analytics.jsx
+++ client/src/components/Analytics.jsx
@@ -3,16 +3,30 @@ import {useBackButton} from "../customHooks/useBackButton"
 import { useRecoilState } from "recoil";
 import { textState } from "../Recoil/recoil_state";
 import Transition from "../Transition";
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 export function Analytics() {
   useBackButton("/");
 
-  const [val,setVal] = useRecoilState(textState);
-  
-  setVal("Analytics")
+  // const [val,setVal] = useRecoilState(textState);
+  // setVal("Analytics")
+
+  const fetcher = async ()=>{
+    const uri = "https://catfact.ninja/fact";
+      const response = await axios.get(uri);
+      console.log(response.data);
+      return response.data
+  }
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: fetcher,
+    refetchOnWindowFocus: true,
+    refetchInterval: 6000,
+  })
+
   return (
     <Transition>
-    {val}
+    {!isLoading && data.fact}
     </Transition>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,14 +8,19 @@ import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Notifications } from "@mantine/notifications";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme="dark">
       <BrowserRouter>
-        <Notifications />
         <RecoilRoot>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <Notifications />
+            <App />
+          </QueryClientProvider>
         </RecoilRoot>
       </BrowserRouter>
     </MantineProvider>
