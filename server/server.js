a5dd6e639b8273467324cdb53978067ca7a974cb
Vishal Kumar <21ucc116@lnmiit.ac.in>
2024-03-27 13:03:52 +0530

    trying bypassing public routes

--- server/server.js
+++ server/server.js
@@ -1,55 +1,55 @@
 // mongodb
 require("./config/db");
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 const bodyParser = express.json;
 const cors = require("cors");
 const { PORT } = process.env;
 
 const app = express();
-const whitelist = ['http://localhost:5173', 'https://wisewaste.vercel.app']; // add your origins here
+const whitelist = ["http://localhost:5173", "https://wisewaste.vercel.app"]; // add your origins here
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
 app.use(bodyParser());
 app.use(cookieParser());
 
-// function bypassPublicPathsFromCookie(req, res, next) {
-//     const publicPaths = ['/admin/approve', '/admin/reject'];
-//     if (publicPaths.includes(req.path)) {
-//         return next();
-//     }
+function bypassPublicPathsFromCookie(req, res, next) {
+  const publicPaths = ["/admin/approve", "/admin/reject"];
+  if (publicPaths.includes(req.path)) {
+    return next();
+  } else {
+    return next();
+    // const token = req.cookies.token;
+    // if (!token) {
+    //     return res.status(401).json({ message: 'Token expired. Please re-login.' });
+    // }
 
-//     // const token = req.cookies.token;
-//     // if (!token) {
-//     //     return res.status(401).json({ message: 'Token expired. Please re-login.' });
-//     // }
-
-//     // // ...rest of your authentication code...
-// }
-
-// app.use(bypassPublicPathsFromCookie);
+    // // ...rest of your authentication code...
+  }
+}
+app.use(bypassPublicPathsFromCookie);
 
 // /authentication/signup
-app.use('/authentication', require('./routes/user.routes'));
-app.use('/otp', require("./routes/userOTP.routes"));
-app.use('/emailverify', require("./routes/emailVerify.routes"));
-app.use('/admin', require("./routes/admin.routes"));
+app.use("/authentication", require("./routes/user.routes"));
+app.use("/otp", require("./routes/userOTP.routes"));
+app.use("/emailverify", require("./routes/emailVerify.routes"));
+app.use("/admin", require("./routes/admin.routes"));
 
-const startApp = () =>{
-    app.listen(PORT, ()=>{
-        console.log(`WiseWaste Backend running on port ${PORT}`);
-    });
+const startApp = () => {
+  app.listen(PORT, () => {
+    console.log(`WiseWaste Backend running on port ${PORT}`);
+  });
 };
 
 startApp();
